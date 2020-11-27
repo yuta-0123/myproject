f913e23f170a9b878e3faa20c97b0c1c240addc8
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-11-28 06:02:21 +0900

    postcssについて

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 const { task } = require('gulp')
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
 
 sass.compiler = require('node-sass');
 
 function cssTranspile(){
     return gulp.src('src/scss/**/*.scss')
+    .pipe(plumber({
+        errorHandler:notify.onError('<%=error.message %>'),
+    }))
+    .pipe(sassGlob())
     .pipe(sass())
-    .on('error',sass.logError)
+    .pipe(postcss([
+        autoprefixer({
+          grid: true,
+        }),
+        cssnano({
+          autoprefixer: false,
+        }),
+      ]))
     .pipe(gulp.dest('dist/css/'))
 }
 
